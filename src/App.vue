66bc6bc5047ace59ee676e3f2030d6bb10e6b35e
earielle <demix3991@gmail.com>
2022-11-27 16:50:35 +0700

    move logic to computed, add watchers

--- src/App.vue
+++ src/App.vue
@@ -45,11 +45,11 @@
               />
             </div>
             <div
-              v-show="suggestions"
-              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+              v-show="listOfSuggestions.length"
+              class="flex bg-white p-1 rounded-md shadow-md flex-wrap"
             >
               <span
-                v-for="(coin, i) in filterSuggests()"
+                v-for="(coin, i) in listOfSuggestions"
                 :key="i"
                 @click="(ticker = coin), add()"
                 class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
@@ -83,15 +83,36 @@
           Добавить
         </button>
       </section>
+      <div>
+        <input
+          v-model="filter"
+          class="block w-24 pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+          type="text"
+          placeholder="filter"
+        />
+        <button
+          v-if="page > 1"
+          @click="page--"
+          class="my-4 inline-flex items-center m-1 py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          Назад</button
+        ><button
+          v-if="hasNextPage"
+          @click="page++"
+          class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+        >
+          Вперед
+        </button>
+      </div>
 
       <hr v-if="tickers.lenght" class="w-full border-t border-gray-600 my-4" />
       <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
         <div
-          v-for="t of tickers"
+          v-for="t of paginatedTickers"
           :key="t"
           @click="select(t)"
           :class="{
-            'border - 4': sel === t,
+            'border - 4': selectedTicker === t,
           }"
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
         >
@@ -125,20 +146,20 @@
         </div>
       </dl>
       <hr v-if="true" class="w-full border-t border-gray-600 my-4" />
-      <section v-if="sel" class="relative">
+      <section v-if="selectedTicker" class="relative">
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ sel.name }} - USD
+          {{ selectedTicker.name }} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div
-            v-for="(bar, ind) in normalizeGraph()"
+            v-for="(bar, ind) in normalizedGraph"
             :key="ind"
             :style="{ height: `${bar}%` }"
             class="bg-purple-800 border w-10"
           ></div>
         </div>
         <button
-          @click="sel = null"
+          @click="selectedTicker = null"
           type="button"
           class="absolute top-0 right-0"
         >
@@ -177,39 +198,97 @@ export default {
     return {
       ticker: "",
       tickers: [],
-      sel: null,
+      selectedTicker: null,
       graph: [],
       coinList: [],
       duplicate: false,
-      suggestions: false,
+      filter: "",
+      page: 1,
     };
   },
   created() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) {
+      this.filter = windowData.filter;
+    }
+
+    if (windowData.page) {
+      this.page = windowData.page;
+    }
+
     const tickerData = localStorage.getItem("keys");
     if (tickerData) {
       this.tickers = JSON.parse(tickerData);
       this.tickers.forEach((t) => this.subscribeToUpdate(t.name));
     }
   },
+  computed: {
+    startIndex() {
+      return (this.page - 1) * 6;
+    },
+    endIndex() {
+      return this.page * 6;
+    },
+    filteredTickers() {
+      return this.tickers.filter((t) =>
+        t.name.includes(this.filter.toUpperCase())
+      );
+    },
+    paginatedTickers() {
+      return this.filteredTickers.slice(this.startIndex, this.endIndex);
+    },
+    hasNextPage() {
+      return this.filteredTickers.length > this.endIndex;
+    },
+    normalizedGraph() {
+      let max = Math.max(...this.graph);
+      let min = Math.min(...this.graph);
+      if (max === min) {
+        return this.graph.map(() => 50);
+      }
+      return this.graph.map((price) => 5 + ((price - min) * 95) / (max - min));
+    },
+    pageOptionsState() {
+      return {
+        page: this.page,
+        filter: this.filter,
+      };
+    },
+    coinsFiltered() {
+      return this.coinList.filter((c) =>
+        c.startsWith(this.ticker.toUpperCase())
+      );
+    },
+    listOfSuggestions() {
+      return this.coinsFiltered
+        .slice()
+        .sort((a, b) => a.length - b.length)
+        .slice(0, 4);
+    },
+  },
   methods: {
     subscribeToUpdate(tickerName) {
       setInterval(async () => {
         try {
           const f = await fetch(
-            `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=921a00875ae85c0d917fd9b549dc605a7a74615e7caa9e3e740a260c4889765f`
+            `https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=cd1a25212c2d593c1b21f31c9381185b1536cf6780df3e5eb9b70dfc8f89aad9`
           );
           const data = await f.json();
+          console.log(data);
           if (this.tickers.find((t) => t.name === tickerName)) {
             this.tickers.find((t) => t.name === tickerName).price =
               data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
           }
-          if (this.sel?.name === tickerName) {
+          if (this.selectedTicker?.name === tickerName) {
             this.graph.push(data.USD);
           }
         } catch (error) {
           console.log(error);
         }
-      }, 3000);
+      }, 5000);
     },
     add() {
       if (this.checkValidInput(this.ticker)) {
@@ -222,24 +301,18 @@ export default {
           this.duplicate = true;
           return;
         }
-        this.tickers.push(newTicker);
+        this.tickers = [...this.tickers, newTicker];
         this.subscribeToUpdate(newTicker.name);
-        let x = JSON.stringify(this.tickers);
-        localStorage.setItem("keys", x);
       }
+    },
+    select(ticker) {
+      this.selectedTicker = ticker;
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
-      this.sel = null;
-    },
-    select(ticker) {
-      this.sel = ticker;
-      this.graph = [];
-    },
-    normalizeGraph() {
-      let max = Math.max(...this.graph);
-      let min = Math.min(...this.graph);
-      return this.graph.map((price) => 5 + ((price - min) * 95) / (max - min));
+      let localValues = JSON.stringify(this.tickers);
+      localStorage.setItem("keys", localValues);
+      this.selectedTicker = null;
     },
     async getCoinlist() {
       const c = await fetch(
@@ -248,20 +321,33 @@ export default {
       const data = await c.json();
       Object.keys(data.Data).forEach((i) => this.coinList.push(i));
     },
-    filterSuggests() {
-      let pred = this.ticker.toUpperCase();
-      let arr = this.coinList.filter((p) => p.startsWith(pred));
-      arr.sort((a, b) => a.length - b.length);
-      arr.length > 4 ? (arr.length = 4) : arr.length;
-      arr.length === 0 ? (this.suggestions = false) : (this.suggestions = true);
-      return arr;
-    },
     checkValidInput(tick) {
       return this.coinList.includes(tick.toUpperCase());
     },
   },
   beforeMount() {
     this.getCoinlist();
+  },
+  watch: {
+    selectedTicker() {
+      this.graph = [];
+    },
+    paginatedTickers() {
+      if (this.paginatedTickers.length === 0 && this.page > 1) this.page -= 1;
+    },
+    tickers() {
+      localStorage.setItem("keys", JSON.stringify(this.tickers));
+    },
+    filter() {
+      this.page = 1;
+    },
+    pageOptionsState() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
   },
 };
 </script>
